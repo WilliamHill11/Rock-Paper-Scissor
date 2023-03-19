f62fbc57c2a8568d88593385d56e8b6fc021512e
william hill <williamhill0831@Hotmail.com>
2023-03-19 02:06:48 -0400

    working on the ui when pressing each button

--- script.js
+++ script.js
@@ -4,32 +4,80 @@ function getComputerChoice() {
     return selection[Math.floor(Math.random() * selection.length)];
 }
 
+const computerSelection = getComputerChoice();
+
+const rock = document.querySelector('#option1');
+const paper = document.querySelector('#option2');
+const scissor = document.querySelector('#option3');
+const container = document.querySelector('#container');
+const content = document.createElement('div');
+const para = document.createElement('p');
+container.appendChild(content);
+        
+
 // Possibilities of which player wins
 function playRound (playerSelection, computerSelection) {
     if (playerSelection === 'rock' && computerSelection === 'scissor') {
-        console.log('Player wins, rock beats scissor')
+        para.textContent = "Rock beats scissor, player wins";
+        content.appendChild(para);
         return playerScore++ 
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
         console.log('Player wins, paper beats rock')
+        const para = document.createElement('p');
+        para.textContent = 'Paper beats rock, player wins';
+        content.appendChild(para);
         return playerScore++
     } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
         console.log('Player wins, scissor beats paper')
+        const para = document.createElement('p');
+        para.textContent = 'Scissor beats paper, player wins';
+        content.appendChild(para);
         return playerScore++
     } else if (computerSelection === 'rock' && playerSelection === 'scissor') {
         console.log('Computer, rock beats scissor')
+        const para = document.createElement('p');
+        para.textContent = 'Rock beat scissor, computer wins';
+        content.appendChild(para);
         return computerScore++
     } else if (computerSelection === 'paper' && playerSelection === 'rock') {
         console.log('Computer wins, paper beats rock')
+        const para = document.createElement('p');
+        para.textContent = 'Paper beats rock, computer wins';
+        content.appendChild(para);
         return computerScore++
     } else if (computerSelection === 'scissor' && playerSelection === 'paper') {
         console.log('Computer wins, scissor beats paper')
+        const para = document.createElement('p');
+        para.textContent = 'Scissor beats paper, computer wins';
+        content.appendChild(para);
         return computerScore++
     } else if (computerSelection === playerSelection) {
+        const para = document.createElement('p');
+        para.textContent = "It's a tie!";
+        content.appendChild(para);
         return 'it\'s a draw'
-    } else {
-        return 'that is not a valid selection'
     }
 }
+
+
+// const playerSelection = prompt('rock, paper, scissor?').toLowerCase();
+
+
+rock.addEventListener('click', function(e) {
+    playRound('rock', computerSelection);
+    para.textContent = '';
+});
+
+paper.addEventListener('click', function(e) {
+    playRound('paper', computerSelection);
+    para.textContent = '';
+});
+
+scissor.addEventListener('click', function(e) {
+    playRound('scissor', computerSelection);
+    para.textContent = '';
+});
+
 
 //Score
 let computerScore = 0;
@@ -38,20 +86,17 @@ let playerScore = 0;
 
 // 5 round game
 function game() {
-    gameFinished = false
+    gameFinished = true
     while (gameFinished) {
-        const playerSelection = prompt('rock, paper, scissor?').toLowerCase();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection))
-        console.log(`Player: ${playerScore}, Computer: ${computerScore}`)
         if (computerScore === 5) {
             console.log('Computer is the almighty champion')
-            gameFinished = true
+            gameFinished = false
         } else if (playerScore === 5) {
             console.log('Player is the almighty champion')
-            gameFinished = true
+            gameFinished = false
         }
     }
 }
 
-game()
+// if (playerSelection === 'rock' && computerSelection === 'scissor' || playerSelection === 'paper' && computerSelection
+// === 'rock' || playerSelection === 'scissor' && computerSelection === 'paper' ) 
