e18612c1bcae7b8cb6856e0e4168511342012d09
William Hill <williamhill@Williams-MacBook-Pro.local>
2023-03-19 15:15:11 -0400

    Made progress on the user interface and made the game work

--- README.md
+++ README.md
@@ -9,3 +9,6 @@
   - Better use of conditionals to get all the different types of options rather than using a bunch of them. 
   - Add styles
   
+Create three buttons, one for each selection. Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
+
+Add a div for displaying results and change all of your console.logs into DOM methods.
--- script.js
+++ script.js
@@ -4,120 +4,134 @@ function getComputerChoice() {
     return selection[Math.floor(Math.random() * selection.length)];
 }
 
-const computerSelection = getComputerChoice();
-
+// Getting all the elements from html
 const rock = document.querySelector('#option1');
 const paper = document.querySelector('#option2');
 const scissor = document.querySelector('#option3');
 const container = document.querySelector('#container');
 const content = document.createElement('div');
 const para = document.createElement('p');
+const restartGame = document.querySelector('#restart');
+
+//Appending div into html container
 container.appendChild(content);
-        
 
-// Possibilities of which player wins
-function playRound (playerSelection, computerSelection) {
-    if (playerSelection === 'rock' && computerSelection === 'scissor') {
-        para.textContent = "Rock beats scissor, player wins";
-        content.appendChild(para);
-        return playerScore++ 
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        console.log('Player wins, paper beats rock')
-        const para = document.createElement('p');
-        para.textContent = 'Paper beats rock, player wins';
-        content.appendChild(para);
-        return playerScore++
-    } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
-        console.log('Player wins, scissor beats paper')
-        const para = document.createElement('p');
-        para.textContent = 'Scissor beats paper, player wins';
-        content.appendChild(para);
-        return playerScore++
-    } else if (computerSelection === 'rock' && playerSelection === 'scissor') {
-        console.log('Computer, rock beats scissor')
-        const para = document.createElement('p');
-        para.textContent = 'Rock beat scissor, computer wins';
-        content.appendChild(para);
-        return computerScore++
-    } else if (computerSelection === 'paper' && playerSelection === 'rock') {
-        console.log('Computer wins, paper beats rock')
-        const para = document.createElement('p');
-        para.textContent = 'Paper beats rock, computer wins';
-        content.appendChild(para);
-        return computerScore++
-    } else if (computerSelection === 'scissor' && playerSelection === 'paper') {
-        console.log('Computer wins, scissor beats paper')
-        const para = document.createElement('p');
-        para.textContent = 'Scissor beats paper, computer wins';
-        content.appendChild(para);
-        return computerScore++
-    } else if (computerSelection === playerSelection) {
-        const para = document.createElement('p');
-        para.textContent = "It's a tie!";
-        content.appendChild(para);
-        return 'it\'s a draw'
-    }
-}
-
-
-// const playerSelection = prompt('rock, paper, scissor?').toLowerCase();
-
-
-rock.addEventListener('click', function(e) {
-    playRound('rock', computerSelection);
-    para.textContent = '';
-});
-
-paper.addEventListener('click', function(e) {
-    playRound('paper', computerSelection);
-    para.textContent = '';
-});
-
-scissor.addEventListener('click', function(e) {
-    playRound('scissor', computerSelection);
-    para.textContent = '';
+//Restart button
+restartGame.addEventListener('click', (e) => {
+    location.reload();
 });
 
 
 //Score
 let computerScore = 0;
 let playerScore = 0;
+        
 
-
-// 5 round game
-function game() {
-    gameFinished = true
-    while (gameFinished) {
-        if (computerScore === 5) {
-            console.log('Computer is the almighty champion')
-            gameFinished = false
-        } else if (playerScore === 5) {
-            console.log('Player is the almighty champion')
-            gameFinished = false
-    
-    //Score
-    let computerScore = 0;
-    let playerScore = 0;
-    
-
-    // 5 round game
-    function game() {
-        gameFinished = true
-        while (gameFinished) {
-            const playerSelection = prompt('rock, paper, scissor?').toLowerCase();
-            const computerSelection = getComputerChoice();
-            console.log(playRound(playerSelection, computerSelection))
-            console.log(`Player: ${playerScore}, Computer: ${computerScore}`)
-            if (computerScore === 5) {
-                console.log('Computer is the almighty champion')
-                gameFinished = false
-            } else if (playerScore === 5) {
-                console.log('Player is the almighty champion')
-                gameFinished = false
-            }
-        }
+// Possibilities of which player wins
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection === 'rock' && computerSelection === 'scissor') {
+        para.textContent = "Rock beats scissor, player wins";
+        content.append(para);
+        return playerScore++ 
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        para.textContent = 'Paper beats rock, player wins';
+        content.append(para);
+        return playerScore++
+    } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
+        para.textContent = 'Scissor beats paper, player wins';
+        content.append(para);
+        return playerScore++
+    } else if (computerSelection === 'rock' && playerSelection === 'scissor') {
+        para.textContent = 'Rock beat scissor, computer wins';
+        content.append(para);
+        return computerScore++
+    } else if (computerSelection === 'paper' && playerSelection === 'rock') {
+        para.textContent = 'Paper beats rock, computer wins';
+        content.append(para);
+        return computerScore++
+    } else if (computerSelection === 'scissor' && playerSelection === 'paper') {
+        para.textContent = 'Scissor beats paper, computer wins';
+        content.append(para);
+        return computerScore++
+    } else if (computerSelection === playerSelection) {
+        para.textContent = "It's a tie!";
+        content.append(para);
+        return 'it\'s a draw'
     }
 }
 
+//Rock selection
+rock.addEventListener('click', function(e) {
+    playRound('rock', getComputerChoice());
+    console.log(`Player: ${playerScore}, Computer: ${computerScore}`)
+    if (computerScore === 5) {
+        para.textContent = 'Computer is the almighty champion';
+        content.append(para);
+        let playAgain = prompt('You want to play another game? type yes or no')
+        if (playAgain === 'yes'.toLowerCase()) {
+                playerScore = 0
+                computerScore = 0
+        }
+    }
+    else if (playerScore === 5) {
+        para.textContent = 'Player is the almighty champion';
+        content.append(para);
+        let playAgain = prompt('You want to play another game? type yes or no')
+        if (playAgain === 'yes'.toLowerCase()) {
+                playerScore = 0
+                computerScore = 0
+        }
+    }
+});
+
+//Paper selection
+paper.addEventListener('click', function(e) {
+    playRound('paper', getComputerChoice());
+    console.log(`Player: ${playerScore}, Computer: ${computerScore}`)
+    if (computerScore === 5) {
+        para.textContent = 'Computer is the almighty champion';
+        content.append(para);
+        let playAgain = prompt('You want to play another game? type yes or no')
+        if (playAgain === 'yes'.toLowerCase()) {
+                playerScore = 0
+                computerScore = 0
+        }
+    }
+    else if (playerScore === 5) {
+        para.textContent = 'Player is the almighty champion';
+        content.append(para);
+        let playAgain = prompt('You want to play another game? type yes or no')
+        if (playAgain === 'yes'.toLowerCase()) {
+                playerScore = 0
+                computerScore = 0
+        }
+    }
+});
+
+//Scissor selection
+scissor.addEventListener('click', function(e) {
+    playRound('scissor', getComputerChoice());
+    console.log(`Player: ${playerScore}, Computer: ${computerScore}`)
+    if (computerScore === 5) {
+        para.textContent = 'Computer is the almighty champion';
+        content.append(para);
+        let playAgain = prompt('You want to play another game? type yes or no')
+        if (playAgain === 'yes'.toLowerCase()) {
+                playerScore = 0
+                computerScore = 0
+        }
+    }
+    else if (playerScore === 5) {
+        para.textContent = 'Player is the almighty champion';
+        content.append(para);
+        let playAgain = prompt('You want to play another game? type yes or no')
+        if (playAgain === 'yes'.toLowerCase()) {
+                playerScore = 0
+                computerScore = 0
+        }
+    }
+});
+
+
 // if (playerSelection === 'rock' && computerSelection === 'scissor' || playerSelection === 'paper' && computerSelection
-// === 'rock' || playerSelection === 'scissor' && computerSelection === 'paper' ) 
+// === 'rock' || playerSelection === 'scissor' && computerSelection === 'paper' )
