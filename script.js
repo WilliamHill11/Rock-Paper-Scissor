36e5cad49423f8f6b015cbd244b368b69f5dbc69
william hill <williamhill0831@Hotmail.com>
2023-03-19 22:37:41 -0400

    added player and computer scoreboard instead of using the console

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ restartGame.addEventListener('click', (e) => {
 
 
 //Score
+const scoreBoard = document.createElement('p');
 let computerScore = 0;
 let playerScore = 0;
         
@@ -30,106 +31,96 @@ let playerScore = 0;
 // Possibilities of which player wins
 function playRound (playerSelection, computerSelection) {
     if (playerSelection === 'rock' && computerSelection === 'scissor') {
-        para.textContent = "Rock beats scissor, player wins";
+        para.textContent = "You win rock beats scissor";
         content.append(para);
         return playerScore++ 
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        para.textContent = 'Paper beats rock, player wins';
+        para.textContent = 'You win paper beats rock';
         content.append(para);
         return playerScore++
     } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
-        para.textContent = 'Scissor beats paper, player wins';
+        para.textContent = 'You win scissor beats paper';
         content.append(para);
         return playerScore++
     } else if (computerSelection === 'rock' && playerSelection === 'scissor') {
-        para.textContent = 'Rock beat scissor, computer wins';
+        para.textContent = 'Computer wins rock beats your scissor';
         content.append(para);
         return computerScore++
     } else if (computerSelection === 'paper' && playerSelection === 'rock') {
-        para.textContent = 'Paper beats rock, computer wins';
+        para.textContent = 'Computer wins paper beats your rock';
         content.append(para);
         return computerScore++
     } else if (computerSelection === 'scissor' && playerSelection === 'paper') {
-        para.textContent = 'Scissor beats paper, computer wins';
+        para.textContent = 'Computer wins scissor beats your paper';
         content.append(para);
         return computerScore++
     } else if (computerSelection === playerSelection) {
         para.textContent = "It's a tie!";
         content.append(para);
-        return 'it\'s a draw'
     }
 }
 
 //Rock selection
 rock.addEventListener('click', function(e) {
     playRound('rock', getComputerChoice());
-    console.log(`Player: ${playerScore}, Computer: ${computerScore}`)
+    scoreBoard.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
+    content.append(scoreBoard);
     if (computerScore === 5) {
-        para.textContent = 'Computer is the almighty champion';
+        para.textContent = 'The computer is the almighty champion';
         content.append(para);
-        let playAgain = prompt('You want to play another game? type yes or no')
-        if (playAgain === 'yes'.toLowerCase()) {
-                playerScore = 0
-                computerScore = 0
-        }
+        alert('You LOST!')
+            playerScore = 0
+            computerScore = 0
     }
     else if (playerScore === 5) {
-        para.textContent = 'Player is the almighty champion';
+        para.textContent = 'You are the almighty champion';
         content.append(para);
-        let playAgain = prompt('You want to play another game? type yes or no')
-        if (playAgain === 'yes'.toLowerCase()) {
-                playerScore = 0
-                computerScore = 0
-        }
+        alert('You Win!')
+            playerScore = 0
+            computerScore = 0
     }
 });
 
 //Paper selection
 paper.addEventListener('click', function(e) {
     playRound('paper', getComputerChoice());
-    console.log(`Player: ${playerScore}, Computer: ${computerScore}`)
+    scoreBoard.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
+    content.append(scoreBoard);
     if (computerScore === 5) {
-        para.textContent = 'Computer is the almighty champion';
+        para.textContent = 'The computer is the almighty champion';
         content.append(para);
-        let playAgain = prompt('You want to play another game? type yes or no')
-        if (playAgain === 'yes'.toLowerCase()) {
-                playerScore = 0
-                computerScore = 0
-        }
+        alert('You LOST!')
+            playerScore = 0
+            computerScore = 0
     }
     else if (playerScore === 5) {
-        para.textContent = 'Player is the almighty champion';
+        para.textContent = 'You are the almighty champion';
         content.append(para);
-        let playAgain = prompt('You want to play another game? type yes or no')
-        if (playAgain === 'yes'.toLowerCase()) {
-                playerScore = 0
-                computerScore = 0
-        }
+        alert('You Win!')
+            playerScore = 0
+            computerScore = 0
     }
 });
 
 //Scissor selection
 scissor.addEventListener('click', function(e) {
     playRound('scissor', getComputerChoice());
-    console.log(`Player: ${playerScore}, Computer: ${computerScore}`)
+    scoreBoard.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
+    content.append(scoreBoard);
     if (computerScore === 5) {
-        para.textContent = 'Computer is the almighty champion';
+        para.textContent = 'The computer is the almighty champion';
         content.append(para);
-        let playAgain = prompt('You want to play another game? type yes or no')
-        if (playAgain === 'yes'.toLowerCase()) {
-                playerScore = 0
-                computerScore = 0
-        }
+        alert('You LOST!')
+            playerScore = 0
+            computerScore = 0
     }
     else if (playerScore === 5) {
-        para.textContent = 'Player is the almighty champion';
+        para.textContent = 'You are the almighty champion';
         content.append(para);
-        let playAgain = prompt('You want to play another game? type yes or no')
-        if (playAgain === 'yes'.toLowerCase()) {
-                playerScore = 0
-                computerScore = 0
-        }
-    }
+        alert('You Win!')
+        playerScore = 0
+        computerScore = 0
+}
 });
 
 
